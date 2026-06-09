14b8e59aa29b53b6350ca2540d1793e56dd8c872
Gowtham A <gowtham2006a@gmail.com>
2026-06-09 21:27:59 +0530

    add MinAndMaxInArray program

---
+++ Day2/MinAndMaxInArray.java
@@ -0,0 +1,31 @@
+package Day2;
+import java.util.Scanner;
+
+public class MinAndMaxInArray {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a size of array: ");
+        int size = sc.nextInt();
+        int arr[]=new int[size];
+        for(int i=0;i<size;i++){
+            System.out.println("Enter the array element: ");
+            arr[i]=sc.nextInt();
+        }
+        int min=arr[0];
+        for(int i=0;i<size;i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+        System.out.println("min of array is: "+min);
+        int max=arr[0];
+        for(int i=0;i<size;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println("max of array is: "+max);
+        sc.close();
+
+    }
+}
