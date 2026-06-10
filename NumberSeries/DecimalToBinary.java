6fbaab48eb2ec79d81b1155b51293b2e29a26cfb
Gowtham A <gowtham2006a@gmail.com>
2026-06-10 21:14:53 +0530

    Add Decimal to Binary and Octal program

---
+++ NumberSeries/DecimalToBinary.java
@@ -0,0 +1,18 @@
+package NumberSeries;
+
+import java.util.Scanner;
+
+public class DecimalToBinary {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a num :");
+        int num = sc.nextInt();
+        String binary = "";
+        while(num>0){
+            binary=(num%2)+binary;
+            num/=2;
+        }
+        System.out.println(binary);
+        sc.close();
+    }
+}
---
+++ NumberSeries/DecimalToOctal.java
@@ -0,0 +1,18 @@
+package NumberSeries;
+
+import java.util.Scanner;
+
+public class DecimalToOctal {
+     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a num :");
+        int num = sc.nextInt();
+        String octal = "";
+        while(num>0){
+            octal=(num%8)+octal;
+            num/=8;
+        }
+        System.out.println(octal);
+        sc.close();
+    }
+}
