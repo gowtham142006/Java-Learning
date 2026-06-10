a9b67cf0eb67d48d65af26a81c8f19357621282e
Gowtham A <gowtham2006a@gmail.com>
2026-06-10 19:32:59 +0530

    Add FrequencyNumberCount program

---
+++ Arrays/FrequencyNumberCount.java
@@ -0,0 +1,33 @@
+package Arrays;
+import java.util.Scanner;
+public class FrequencyNumberCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a array size");
+        int size = sc.nextInt();
+        int arr[]=new int[size];
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<size;i++){
+            int count=1;
+            int flag=0;
+
+            for(int j=0;j<i;j++){
+                if(arr[i]==arr[j]){
+                    flag=1;
+                }
+            }
+            
+            if(flag==0){
+                for(int k=i+1;k<size;k++){
+                    if(arr[k]==arr[i]){
+                        count++;
+                    }
+                }
+                System.out.println(arr[i] +"-"+ count);
+            }
+    }
+        sc.close();
+    }
+}
