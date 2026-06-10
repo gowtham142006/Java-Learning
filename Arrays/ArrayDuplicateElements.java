340cd9924a13966c9e1c3ec1deaf9cb90dccd610
Gowtham A <gowtham2006a@gmail.com>
2026-06-10 20:41:32 +0530

    Add Duplicate element program

---
+++ Arrays/ArrayDuplicateElements.java
@@ -0,0 +1,38 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class ArrayDuplicateElements {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a size of array :");
+        int size = sc.nextInt();
+        int arr[]=new int[size];
+        for(int i=0;i<size;i++){
+            System.out.println("Enter a element of array "+i+":");
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<size;i++){
+            int count=1;
+            int flag=0;
+
+            for(int j=0;j<i;j++){
+                if(arr[i]==arr[j]){
+                    flag=1;
+                }
+            }
+            
+            if(flag==0){
+                for(int k=i+1;k<size;k++){
+                    if(arr[k]==arr[i]){
+                        count++;
+                    }
+                }
+                if(count>1){
+                System.out.println("Duplicate elements are: "+arr[i]);
+            }
+        }
+    }
+    sc.close();
+    }
+}
