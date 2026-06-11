6e59d785aedeef866136f502e3d6ec048831a72f
Gowtham A <gowtham2006a@gmail.com>
2026-06-11 19:51:09 +0530

    Add Privious num greater and lesser

---
+++ Arrays/PriviousNumGreater.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class PriviousNumGreater {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a size of array :");
+        int size = sc.nextInt();
+        int arr[]=new int[size];
+        for(int i=0;i<size;i++){
+            System.out.println("Enter a element of array "+i+":");
+            arr[i]=sc.nextInt();
+        }
+        int max=arr[0];
+        System.out.println(max);
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+                System.out.println(max);
+            }
+        }
+        sc.close();
+}
+}
