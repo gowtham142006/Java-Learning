9b3934e7e09089787a477c39180260506023824e
Gowtham A <gowtham2006a@gmail.com>
2026-06-12 14:21:42 +0530

    Add Vowels And Consonent Count program

---
+++ String/VowelsAndConsonentCount.java
@@ -0,0 +1,27 @@
+package String;
+
+import java.util.Scanner;
+
+public class VowelsAndConsonentCount {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        String str = sc.nextLine();
+        int v=0;
+        int c=0;
+        for(int i=0;i<str.length();i++){
+            char ch =str.charAt(i);
+            if(ch>='a'&&ch<='z'||ch>='A'&&ch<='Z'){
+                if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
+                    v++;
+                }
+                else{
+                    c++;
+                }
+            }
+        }
+        System.out.println(v);
+        System.out.println(c);
+        sc.close();
+
+    }
+}
