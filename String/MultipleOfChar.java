e45a648d982d9d8bd15e880b70aaeb3b1f904188
Gowtham A <gowtham2006a@gmail.com>
2026-06-13 13:59:18 +0530

    Add Multiple Of Char

---
+++ String/MultipleOfChar.java
@@ -0,0 +1,24 @@
+package String;
+
+import java.util.Scanner;
+
+public class MultipleOfChar {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int i=0;
+        while ( i<str.length()) {
+            char ch =str.charAt(i);
+            i++;
+            int count=0;
+            while(i<str.length()&&(str.charAt(i)>='0'&&str.charAt(i)<='9')){
+                count=count*10+(str.charAt(i)-'0');
+                i++;
+            }
+            for(int k=0;k<count;k++){
+                System.out.print(ch);
+        }
+        }
+        sc.close();
+    }
+}
