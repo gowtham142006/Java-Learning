fcb719fb046f858272a6151ac530cdf0df20dfb9
Gowtham A <gowtham2006a@gmail.com>
2026-06-15 11:33:59 +0530

    Add Anagram Or Not

---
+++ String/AnagramOrNot.java
@@ -0,0 +1,33 @@
+package String;
+
+import java.util.Scanner;
+
+public class AnagramOrNot {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+        int arr[] = new int[254];
+
+        for(int i=0;i<str1.length();i++){
+            arr[str1.charAt(i)]++;
+        }
+        for(int i=0;i<str2.length();i++){
+            arr[str2.charAt(i)]--;
+        }
+        int flag=0;
+        for(int k=0;k<254;k++){
+            if(arr[k]!=0){
+                flag=1;
+                break;
+            }
+        }
+        if(flag==0){
+            System.out.println("Anagram");
+        }
+        else{
+            System.out.println("Not Anagram");
+        }
+sc.close();
+    }
+}
