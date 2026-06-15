882015b99178df6282d4dbc2ad49776897bf6e15
Gowtham A <gowtham2006a@gmail.com>
2026-06-15 14:16:58 +0530

    Add Print Extra Character

---
+++ String/PrintExtraChar.java
@@ -0,0 +1,22 @@
+package String;
+
+import java.util.Scanner;
+
+public class PrintExtraChar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1=sc.nextLine();
+        String str2=sc.nextLine();
+        int sum1=0;
+        int sum2=0;
+        
+        for(int i=0;i<str1.length();i++){
+            sum1=sum1+str1.charAt(i);
+        }
+        for(int j=0;j<str2.length();j++){
+            sum2=sum2+str2.charAt(j);
+        }
+         char ch= (char)(sum2-sum1);
+        System.out.print(ch);
+    }
+}
