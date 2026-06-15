231961ac05f2351202f019e2744b07203415c08b
Gowtham A <gowtham2006a@gmail.com>
2026-06-15 11:59:43 +0530

    Add Panagram Or Not

---
+++ String/PanagramOrNot.java
@@ -0,0 +1,28 @@
+package String;
+
+import java.util.Scanner;
+
+public class PanagramOrNot {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int count =0;
+        String str1=str.toLowerCase();
+        for(char ch='a';ch<='z';ch++){
+        for(int i=0;i<str.length();i++){
+            char ch1=str1.charAt(i);
+            if(ch==ch1){
+                count++;
+                break;
+            }
+        }
+    }
+    if(count==26){
+        System.out.println("Panagram");
+    }
+    else{
+        System.out.println("Not Panagram");
+
+    }
+    sc.close();
+}}
