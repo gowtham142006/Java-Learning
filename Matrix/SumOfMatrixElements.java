a18574cfdaa5a5b414706dedbf27321c5f0c319e
Gowtham A <gowtham2006a@gmail.com>
2026-06-16 14:06:23 +0530

    Add Sum Of Matrix Elements

---
+++ Matrix/SumOfMatrixElements.java
@@ -0,0 +1,29 @@
+package Matrix;
+
+import java.util.Scanner;
+
+public class SumOfMatrixElements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r=sc.nextInt();
+        int c = sc.nextInt();
+        int arr[][]=new int[r][c];
+        int sum=0;
+        System.out.println("Enter a elements :");
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                sum+=arr[i][j];
+            }
+        }
+        System.out.println("Sum of matrix is ");
+        System.out.println(sum);
+        sc.close();
+    }
+}
+
+
