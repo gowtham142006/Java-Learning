5d9426efe50c5fdba9dc2e49dd5c57aafa0588bc
Gowtham A <gowtham2006a@gmail.com>
2026-06-16 11:26:58 +0530

    Add Isomorphic Or Not

---
+++ String/Isomorphic.java
@@ -0,0 +1,32 @@
+package String;
+
+import java.util.Scanner;
+
+public class Isomorphic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string :");
+        String str =sc.nextLine();
+        System.out.println("Enter a string :");
+        String str1 =sc.nextLine();
+        int map1[]=new int[254];
+        int map2[]=new int[254];
+        for(int i=0;i<str.length();i++){
+            char ch =str.charAt(i);
+            char ch1 =str1.charAt(i);
+            if(map1[ch]==0&&map2[ch1]==0){
+                map1[ch]=ch1;
+                map2[ch1]=ch;
+            }
+            else if(map1[ch]!=ch1||map2[ch1]!=ch){
+                System.out.println("Not Isomarphic");
+                break;
+            }
+            else{
+                System.out.println("Isomarphic");
+            }
+        }
+        sc.close();
+
+    }
+}
