8a6570592de607ff4cb7705bc1ae45c3afb615fe
Gowtham A <gowtham2006a@gmail.com>
2026-06-17 10:46:39 +0530

    Add Right Shift Of Array

---
+++ Arrays/RightShiftOfArray.java
@@ -0,0 +1,26 @@
+package Arrays;
+import java.util.Scanner;
+public class RightShiftOfArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a size of array :");
+         int size = sc.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter no of rotation");
+        int k = sc.nextInt();
+        k=k%size;
+        int r[]=new int[size];
+        for(int i=0;i<size;i++){
+            r[(i+k)%size]=arr[i];
+        }
+        System.out.println("Array k times shift is");
+        for(int i=0;i<size;i++){
+            System.out.println(r[i]+" ");
+        }
+        sc.close();
+    }
+}
