a7afa3843360c03bf7404841da392a0a58f12f67
Gowtham A <gowtham2006a@gmail.com>
2026-06-17 14:13:29 +0530

    Add Change90DegreeOf Matrix

---
+++ Matrix/Chang90DegreeOfMatrix.java
@@ -0,0 +1,28 @@
+package Matrix;
+
+import java.util.Scanner;
+
+public class Chang90DegreeOfMatrix {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a row size: ");
+        int r = sc.nextInt();
+        System.out.println("Enter a column size: ");
+        int c = sc.nextInt();
+        int arr[][]=new int[r][c];
+        System.out.println("Enter Element of matrix: ");
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        
+        for(int i=0;i<r;i++){
+            for(int j=c-1;j>=0;j--){
+                System.out.print(arr[j][i]+" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
