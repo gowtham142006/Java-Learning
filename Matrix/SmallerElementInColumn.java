97ded48bcd97d681ead8a86f9b7bf1a2ae85ebea
Gowtham A <gowtham2006a@gmail.com>
2026-06-17 13:51:01 +0530

    Add Smaller Element In Column

---
+++ Matrix/SmallerElementInColumn.java
@@ -0,0 +1,31 @@
+package Matrix;
+
+import java.util.Scanner;
+
+public class SmallerElementInColumn {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a row size: ");
+        int r = sc.nextInt();
+        System.out.println("Enter a column size: ");
+        int c = sc.nextInt();
+        int arr1[][]=new int[r][c];
+        System.out.println("Enter Element of matrix: ");
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                arr1[i][j]=sc.nextInt();
+            }
+        }
+        
+        for(int i=0;i<r;i++){
+            int min=arr1[0][i];
+            for(int j=0;j<c;j++){
+                if(arr1[j][i]<min){
+                    min=arr1[j][i];
+                }
+            }
+            System.out.print(min+" ");
+        }
+        sc.close();
+    }
+}
