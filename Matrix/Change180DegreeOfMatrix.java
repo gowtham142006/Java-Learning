fb7a521e47812d101ad76f4240ff5f7e08ae34c5
Gowtham A <gowtham2006a@gmail.com>
2026-06-18 11:52:42 +0530

    Add 180 Degree Of Matrix

---
+++ Matrix/Change180DegreeOfMatrix.java
@@ -0,0 +1,24 @@
+package Matrix;
+
+import java.util.Scanner;
+
+public class Change180DegreeOfMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r=sc.nextInt();
+        int c=sc.nextInt();
+        int arr[][]=new int[r][c];
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=r-1;i>=0;i--){
+            for(int j=c-1;j>=0;j--){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
