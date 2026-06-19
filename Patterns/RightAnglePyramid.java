6744ae06d5f1ac43f0415aae22af41e22a886cad
Gowtham A <gowtham2006a@gmail.com>
2026-06-19 12:00:38 +0530

    Add Right Angle Pyramid

---
+++ Patterns/RightAnglePyramid.java
@@ -0,0 +1,17 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class RightAnglePyramid {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n =sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
