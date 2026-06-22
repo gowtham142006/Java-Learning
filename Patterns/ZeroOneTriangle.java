1a0fb70a1e47f39e3286ae2be8cb6f2590a4efd4
Gowtham A <gowtham2006a@gmail.com>
2026-06-22 19:35:14 +0530

    Zero And One Triangle

---
+++ Patterns/ZeroOneTriangle.java
@@ -0,0 +1,30 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class ZeroOneTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            int num;
+            if (i % 2 == 1) {
+                num = 1;
+            } else {
+                num = 0;
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num + " ");
+                num = 1 - num;
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
