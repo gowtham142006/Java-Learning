383bb83dd369dbdb2b14ad5f5b743acdd1416aaa
Gowtham A <gowtham2006a@gmail.com>
2026-06-22 19:29:28 +0530

    Hollow square

---
+++ Patterns/HollowSquare.java
@@ -0,0 +1,33 @@
+package Patterns;
+import java.util.Scanner;
+
+public class HollowSquare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (i == 0 || i == n - 1 || j == 0 || j == n - 1) {
+                    System.out.print("1 ");
+                }
+                else if (n % 2 == 1 && i == n / 2 && j == n / 2) {
+                    System.out.print("1 ");
+                }
+                else if (n % 2 == 0 &&
+                         (i == n / 2 - 1 || i == n / 2) &&
+                         (j == n / 2 - 1 || j == n / 2)) {
+                    System.out.print("1 ");
+                }
+                else {
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
