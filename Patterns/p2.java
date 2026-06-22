cf65f876b76444d688e93d97b00c3d5cc706a7c6
Gowtham A <gowtham2006a@gmail.com>
2026-06-22 16:49:36 +0530

    Patten 2

---
+++ Patterns/p2.java
@@ -0,0 +1,23 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class p2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r =sc.nextInt();
+        int c =sc.nextInt();
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(i==0||j==0||i==r-1||j==c-1){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
