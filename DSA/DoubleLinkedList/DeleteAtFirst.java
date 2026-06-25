6e6de598cf99430a68500d885e5c176729a9dc7d
Gowtham A <gowtham2006a@gmail.com>
2026-06-25 20:41:14 +0530

    Delete At First

---
+++ DSA/DoubleLinkedList/DeleteAtFirst.java
@@ -0,0 +1,66 @@
+package DSA.DoubleLinkedList;
+
+class Node {
+    int data;
+    Node next;
+    Node pre;
+
+    Node(int data){
+        this.data= data;
+        this.next=null;
+        this.pre=null;
+    }
+}
+
+public class DeleteAtFirst {
+     Node head=null;
+
+    void InsertEnd(int data){
+        Node newnode=new Node(data);
+        if(head==null){
+            head=newnode;
+            return;
+        }
+        Node temp=head;
+        while (temp.next!=null) {
+            temp=temp.next;
+        }
+        temp.next=newnode;
+        newnode.pre=temp;
+    }
+
+    void DeleteAtFirstPos(){
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+         if (head.next == null) {
+            head = null;
+            return;
+        }
+        head=head.next;
+        head.pre=null;
+
+    }
+    void display(){
+        Node temp=head;
+    
+        while(temp!=null){
+            System.out.print(temp.data+" -> ");
+            temp=temp.next;
+        }
+        System.out.print("null");
+        System.out.println();
+    }
+   
+    public static void main(String[] args) {
+        DeleteAtFirst obj=new DeleteAtFirst();
+        obj.InsertEnd(10);
+        obj.InsertEnd(20);
+        obj.InsertEnd(30);
+        obj.DeleteAtFirstPos();
+        obj.display();
+        
+    }
+
+}
